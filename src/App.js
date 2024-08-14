0da3e6067df6d50350170861da02a38f551660be
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-08-14 18:40:11 -0400

    added todo list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
-import './App.css'; // http://localhost:9000/todos - myawesomeapi.com/api/todos
+import logo from "./logo.svg";
+import "./App.css"; // http://localhost:9000/todos - myawesomeapi.com/api/todos
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodo = () => {
+    const todo = { id: Date.now(), text: text };
+    setTodos([...todos, todo]);
+    setText("");
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((t) => t.id !== id));
+  };
+
   return (
     <div className="App">
       <h1>My App</h1>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={addTodo}>Add</button>
+      {todos.map((todo) => (
+        <div onClick={() => deleteTodo(todo.id)} key={todo.id}>
+          {todo.text}
+        </div>
+      ))}
     </div>
   );
 }
