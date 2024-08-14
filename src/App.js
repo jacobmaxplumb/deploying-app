084972fd07e498f299274f9905650d04f2433ac6
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-08-14 18:30:44 -0400

    something else

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import logo from './logo.svg';
 import './App.css'; // http://localhost:9000/todos - myawesomeapi.com/api/todos
 
 function App() {
-  throw new Error('');
   return (
     <div className="App">
-      
+      <h1>My App</h1>
     </div>
   );
 }
